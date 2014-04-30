05d12309a1de8607610136dfc87345981824ed5e
Balaji Iyer <balaji.iyer@rackspace.com>
2014-04-30 09:53:28 -0400

    Adding console logs for workers

--- samosa/workers/worker.py
+++ samosa/workers/worker.py
@@ -10,7 +10,9 @@ def main():
     r = init_redis()
     while (1):
         for member in r.smembers('members'):
+            print 'Collecting data for - ' + member
             person = r.hgetall(member)
+
             if person['device'] == "fitbit":
                 creds = eval(person['credentials'])
                 authd_client = fitbit.Fitbit(**creds)
@@ -21,18 +23,19 @@ def main():
                         {"caloriesBMR": response['summary']['caloriesBMR']})
                 r.hmset(time.strftime("%d%m%Y") + member,
                         {"distances": response['summary']['distances'][0]['distance']})
+
             if person['device'] == "jawbone-up":
                 http_url = 'https://jawbone.com/nudge/api/v.1.1/users/@me/moves'
                 headers = eval(person['credentials'])
                 headers['Accept-Header'] = "application/json"
                 response = requests.get(http_url, headers=headers)
                 resp_body = json.loads(response.content)
-                # import pdb
-                # pdb.set_trace()
+
                 r.hmset(time.strftime("%d%m%Y") + member,
                         {"steps": resp_body['data']['items'][0]['details']['steps'],
                          "caloriesBMR": resp_body['data']['items'][0]['details']['bmr'],
                          "distances": 1.6 * resp_body['data']['items'][0]['details']['km']})
+        print 'Sleeping...'
         time.sleep(1800)
 
 
